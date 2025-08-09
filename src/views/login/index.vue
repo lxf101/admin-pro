241158f63d46d18b2338983b19255e3be3d2ab17
lxf101 <lixinfang101@126.com>
2025-08-09 10:14:23 +0800

    feat: commit new

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -20,7 +20,7 @@
                     ></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" style="width: 100%" @click="userLogin">登录</el-button>
+                    <el-button type="primary" style="width: 100%" @click="userLogin">登录1</el-button>
                 </el-form-item>
             </el-form>
         </div>
